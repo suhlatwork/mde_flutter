96cefeb46bb0d35e861986b69fd70c0305f7074d
Sebastian Uhl <sebastian.uhl@mytum.de>
2019-03-04 12:19:17 +0100

    Remove custom style
    Remove all changes to the default style. For the moment they seem to
    be in the way of simply testing the app without extending them for
    each newly used widget. This means the color scheme of the scaffold
    and the web view do not blend well with each other.

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,5 @@ import 'main_viewer.dart';
 
 void main() => runApp(MaterialApp(
       title: 'mods.de Forum',
-      theme: ThemeData(
-        primaryColor: Color(0xff111723),
-        scaffoldBackgroundColor: Color(0xff111723),
-        textTheme: TextTheme(
-          title: TextStyle(color: Colors.white),
-          subtitle:
-              TextStyle(color: Colors.white70, fontWeight: FontWeight.normal),
-        ),
-      ),
       home: MainViewer(),
     ));
